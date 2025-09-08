1595f639351a22b8ab5e8a46124eec0d24e88f66
Nithin KV <gowdanithin232@gmail.com>
2025-09-08 22:55:59 +0530

    Add files via upload

---
+++ Operators/LogicalOperator.java
@@ -0,0 +1,8 @@
+public class LogicalOperator {
+    public static void main(String[] args) {
+    //System.out.println((3>2)&&(5>2));
+    //System.out.println((3>2)||(1>2));
+    //System.out.println(!(3>2));
+}
+
+}
---
+++ Operators/Unary.java
@@ -0,0 +1,11 @@
+public public class Unary {
+public static void main(String[] args) {
+    int a = 10;
+    int b = ++a;
+    System.out.println(a);
+    System.out.println(b);
+} 
+}
+ {
+    
+}
