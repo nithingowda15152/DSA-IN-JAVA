2edfc063fa476f263421e43cf4de995251276040
Nithin KV <gowdanithin232@gmail.com>
2025-09-15 08:50:26 +0530

    Add files via upload

---
+++ Functions&Methods/ReturnType.java
@@ -0,0 +1,14 @@
+public class ReturnType {
+    public static int factorial(int n){
+        int f=1;
+        for(int i=1;i<=n;i++){
+           f=f*i;
+        }
+        return f;
+    }
+    public static void main(String[]args){
+        int f = factorial(3);
+        System.out.println(f);
+
+    }
+}
