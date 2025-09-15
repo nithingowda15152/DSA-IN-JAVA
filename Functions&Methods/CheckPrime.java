5ae4ab0599e56b00eff4887db6a8236384da9bcc
Nithin KV <gowdanithin232@gmail.com>
2025-09-15 22:32:16 +0530

    Add files via upload

---
+++ Functions&Methods/CheckPrime.java
@@ -0,0 +1,17 @@
+public class CheckPrime {
+        public static boolean isprime(int n){
+        if(n<2){
+            return false;
+        }
+        for(int i=2;i<=n-1;i++){
+            if(n%i==0){
+                return false;
+            }
+
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(isprime(2));
+}
+}
