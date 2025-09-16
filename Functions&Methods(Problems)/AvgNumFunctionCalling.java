a2176da188b80e44aa6243d793a71655428e209f
Nithin KV <gowdanithin232@gmail.com>
2025-09-16 09:17:13 +0530

    Add files via upload

---
+++ Functions&Methods(Problems)/AvgNumFunctionCalling.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class AvgNumFunctionCalling {
+    public static void AvgNum(){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        System.out.println(a+b+c);
+    }
+    public static void main(String[] args) {
+        AvgNum();
+    }
+}
